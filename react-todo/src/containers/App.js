049ebad0151e5cf12f4b9860ebacf5abc3b438e9
kimseonmi <zlatjs2@gmail.com>
2017-12-16 12:44:18 +0900

    todo example

---
+++ react-todo/src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <input type="text" />
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
